9f80ac5567ec3e24f0623b4cad1a800f5fd7ae3f
DrNimnoms <nimaghods@Nimas-MacBook-Pro-2.local>
2014-12-07 14:23:10 -0800

    pressure constant

--- BMU/BMU.h
+++ BMU/BMU.h
@@ -41,7 +41,8 @@
 //BMU ADC conversion constants
   #define curConst 0.08587        //80/V*3.3V/4095*4.01ohm/3.01ohm  sensor resolution*adc resolution*voltage divider
   #define volConst 0.0482  //.0439    //(174Kohm+10Kohm)/10Kohm*(10kohm+5.1kohm)/5.1kohm*3.3V/4095
-  #define presConst 0.0061034   //1 kpa/5V/0.018*4.7ohm/3.2ohm*3.3V/4095*0.14503 gpsi/kpa
+//  #define presConst 0.0061034   //1 kpa/5V/0.018*4.7ohm/3.2ohm*3.3V/4095*0.14503 gpsi/kpa
+  #define presConst 0.0012986 
   #define presOffset 0.0058016     //0.04 kpa 0.14503 gpsi/kpa
   #define capConst 0.0000555556    //0.2 sec==>.000055556 hours
 
